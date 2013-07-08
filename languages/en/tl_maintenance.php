086897fc1d9cdfbe474956913e517ec5068d86f5
Alexander Wörndl <alex@madeyourday.co>
2013-07-08 12:41:47 +0200

    Added english translation

---
+++ languages/en/tl_maintenance.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Copyright MADE/YOUR/DAY OG <mail@madeyourday.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * RockSolid Custom Elements maintenance job translations
+ *
+ * @author Martin Auswöger <martin@madeyourday.net>
+ */
+
+$GLOBALS['TL_LANG']['tl_maintenance_jobs']['rocksolid_custom_elements'][0] = 'Clear custom elements cache';
+$GLOBALS['TL_LANG']['tl_maintenance_jobs']['rocksolid_custom_elements'][1] = 'Deletes the saved DCA files of all RockSolid Custom Elements.';
