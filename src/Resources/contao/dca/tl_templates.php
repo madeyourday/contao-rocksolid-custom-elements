e4362170b786cb9d86025aed4215aa0908c4acc2
Martin Auswöger <martin@auswoeger.com>
2020-02-10 18:48:32 +0100

    Fixed #116 bug with php files in template editor

---
+++ src/Resources/contao/dca/tl_templates.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Copyright MADE/YOUR/DAY OG <mail@madeyourday.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * RockSolid Custom Elements DCA
+ *
+ * @author Martin Auswöger <martin@madeyourday.net>
+ */
+
+use Contao\Config;
+use Contao\CoreBundle\Util\PackageUtil;
+
+if (
+	is_callable([PackageUtil::class, 'getContaoVersion'])
+	&& version_compare(PackageUtil::getContaoVersion(), '4.7', '>=')
+) {
+	$GLOBALS['TL_DCA']['tl_templates']['config']['validFileTypes'] .= ',php';
+	$GLOBALS['TL_DCA']['tl_templates']['config']['onload_callback'][] = function() {
+		Config::set('editableFiles', Config::get('editableFiles') . ',php');
+	};
+}
