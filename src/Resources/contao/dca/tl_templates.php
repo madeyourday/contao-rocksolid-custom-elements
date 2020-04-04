3c5d006ef0bb240d77a534d8257fc97b4d28b1b8
Sebastian <55794780+zoglo@users.noreply.github.com>
2020-04-04 23:09:33 +0200

    Fixed compatibility with templates editor for Contao 4.9.2 (#119)

--- src/Resources/contao/dca/tl_templates.php
+++ src/Resources/contao/dca/tl_templates.php
@@ -19,8 +19,20 @@ if (
 	is_callable([PackageUtil::class, 'getContaoVersion'])
 	&& version_compare(PackageUtil::getContaoVersion(), '4.7', '>=')
 ) {
-	$GLOBALS['TL_DCA']['tl_templates']['config']['validFileTypes'] .= ',php';
+	if (!empty($GLOBALS['TL_DCA']['tl_templates']['config']['validFileTypes'])) {
+		$GLOBALS['TL_DCA']['tl_templates']['config']['validFileTypes'] .= ',php';
+	}
 	$GLOBALS['TL_DCA']['tl_templates']['config']['onload_callback'][] = function() {
 		Config::set('editableFiles', Config::get('editableFiles') . ',php');
+	};
+	$originalButtonCallback = $GLOBALS['TL_DCA']['tl_templates']['list']['operations']['source']['button_callback'];
+	$GLOBALS['TL_DCA']['tl_templates']['list']['operations']['source']['button_callback'] = function($row, $href, $label, $title, $icon, $attributes) use ($originalButtonCallback) {
+		if (substr(basename($row['id']), 0, 5) !== 'rsce_' || substr($row['id'], -11) !== '_config.php') {
+			if (is_array($originalButtonCallback)) {
+				return System::importStatic($originalButtonCallback[0])->{$originalButtonCallback[1]}($row, $href, $label, $title, $icon, $attributes);
+			}
+			return $originalButtonCallback($row, $href, $label, $title, $icon, $attributes);
+		}
+		return '<a href="' . \Contao\Backend::addToUrl($href . '&amp;id=' . $row['id']) . '" title="' . Contao\StringUtil::specialchars($title) . '"' . $attributes . '>' . Contao\Image::getHtml($icon, $label) . '</a> ';
 	};
 }
