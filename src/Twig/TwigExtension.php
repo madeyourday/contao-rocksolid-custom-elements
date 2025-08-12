dfb9b5a3e9216bec8265b25555aa0fa13ace4f1e
Martin Auswöger <martin@auswoeger.com>
2025-08-12 17:15:13 +0200

    Twig function inline_svg

--- src/Twig/TwigExtension.php
+++ src/Twig/TwigExtension.php
@@ -2,11 +2,23 @@
 
 namespace MadeYourDay\RockSolidCustomElements\Twig;
 
+use Contao\CoreBundle\Image\Studio\Figure;
+use Contao\CoreBundle\Image\Studio\Studio;
+use Contao\FilesModel;
+use Contao\Image\ImageInterface;
+use Contao\ImagineSvg\Imagine;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 final class TwigExtension extends AbstractExtension
 {
+    public function __construct(
+        private readonly Imagine $imagineSvg,
+        private readonly Studio $imageStudio,
+    ) {
+    }
+
     public function getFilters(): array
     {
         return [
@@ -17,6 +29,36 @@ final class TwigExtension extends AbstractExtension
                         ?? throw new \RuntimeException(\sprintf('Error in regular expression "%s".', $pattern))
                     ;
                 },
+            ),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction(
+                'inline_svg',
+                function (Figure|FilesModel|ImageInterface|int|string|null $svg): string {
+                    if (!$svg instanceof Figure) {
+                        $svg = $this->imageStudio
+                            ->createFigureBuilder()
+                            ->from($svg)
+                            ->buildIfResourceExists();
+                    }
+
+                    if (!$svg) {
+                        return '';
+                    }
+
+                    try {
+                        $dom = $this->imagineSvg->open($svg->getImage()->getFilePath(true))->getDomDocument();
+                        return $dom->saveXML($dom->documentElement);
+                    } catch (\Throwable $e) {
+                    }
+
+                    return '';
+                },
+                ['is_safe' => ['html']],
             ),
         ];
     }
