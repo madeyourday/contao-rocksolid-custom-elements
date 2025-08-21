368014bf1bba63f9d16edd45098e0613c49d2dd7
Martin Auswöger <martin@auswoeger.com>
2025-08-21 09:23:12 +0200

    Remove unnecessary preg_replace Twig filter

--- src/Twig/TwigExtension.php
+++ src/Twig/TwigExtension.php
@@ -8,7 +8,6 @@ use Contao\FilesModel;
 use Contao\Image\ImageInterface;
 use Contao\ImagineSvg\Imagine;
 use Twig\Extension\AbstractExtension;
-use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 final class TwigExtension extends AbstractExtension
@@ -17,20 +16,6 @@ final class TwigExtension extends AbstractExtension
 		private readonly Imagine $imagineSvg,
 		private readonly Studio $imageStudio,
 	) {
-	}
-
-	public function getFilters(): array
-	{
-		return [
-			new TwigFilter(
-				'preg_replace',
-				static function (string|array $subject, string|array $pattern, string|array $replacement, int $limit = -1): string|array {
-					return preg_replace($pattern, $replacement, $subject, $limit)
-						?? throw new \RuntimeException(\sprintf('Error in regular expression "%s".', $pattern))
-					;
-				},
-			),
-		];
 	}
 
 	public function getFunctions(): array
